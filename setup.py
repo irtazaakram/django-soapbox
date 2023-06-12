364f71ec82e35a69fd6f9f52fb257b52cae1ff56
irtazaakram <irtaza.akram@arbisoft.com>
2023-06-12 10:50:57 +0500

    Drop support for python 3.7

--- setup.py
+++ setup.py
@@ -33,11 +33,10 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Topic :: Utilities",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=["Django>=2.2"],
 )
